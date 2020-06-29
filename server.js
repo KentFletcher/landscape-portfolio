48568b5a1379b582ad5174db82deda648b2d3582
KentFletcher <kentfletch09@gmail.com>
2020-06-29 15:36:48 -0700

    fixed bug

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const PORT = 3000;
 app.use(express.static('./public'));
 
 app.get('/hello', (request, response) => {
-  response.status(200).send('Hello in there')
+  response.status(200).send('Hello in there');
 });
 
 app.use('*', (request, response) => response.send('Sorry, that route does not exist'));
 
-app.listen(PORT, () => 
+app.listen(PORT, () =>
   console.log(`server is running on PORT: ${PORT}`)
 );
